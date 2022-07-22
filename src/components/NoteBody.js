27586a58d88b57bfdd280bd3006d1239e5f43063
erwinprasetyo97 <erwinsetyoprasetyo@gmail.com>
2022-07-22 21:14:19 +0700

    update note app

--- src/components/NoteBody.js
+++ src/components/NoteBody.js
@@ -39,7 +39,7 @@ class NoteBody extends React.Component {
           <div className='note-app__body'>
               <NoteInput addNote={this.onAddNoteHandler}/>
               <h2>Catatan Aktif</h2>
-              <NotesList notes={this.state.notes} onDelete={this.onDeleteHandler}/>
+              {this.state.notes.length !== 0 ? <NotesList notes={this.state.notes} onDelete={this.onDeleteHandler}></NotesList> : <div className='empty_note'>Belum ada catatan</div>}
               <h2>Arsip</h2>
           </div>
         )
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -13,6 +13,7 @@ const NotesList = ({notes, onDelete}) => {
                 {...note}/>
             ))
         }
+        
     </div>
   );
 }
